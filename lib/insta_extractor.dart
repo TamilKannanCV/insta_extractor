9e84d0350b15c788ffe3a6444dc93ea84b31fe08
TamilKannanCV <tamilkk100@gmail.com>
2022-08-14 17:59:06 +0530

    export updated

--- lib/insta_extractor.dart
+++ lib/insta_extractor.dart
@@ -3,3 +3,4 @@ export 'src/models/insta/insta.dart';
 export 'src/models/insta/user.dart';
 export 'src/models/insta/video_url.dart';
 export 'src/models/insta/image_url.dart';
+export 'src/utils/link_utils.dart';
